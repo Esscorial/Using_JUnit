a24523d0025ce124bb27b51f85da417c5882c7ce
Esscorial <98604447+Esscorial@users.noreply.github.com>
2022-03-17 23:47:41 +0500

    Update Tests.java

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class CalculatorTest {
+class Test {
     Calculator calc;
 
     @BeforeEach
